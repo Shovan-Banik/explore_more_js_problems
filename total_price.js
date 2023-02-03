fbbcdc8ae6831a4f8517e8a0b0b6a05ab14cf2d8
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-03 17:30:11 +0600

    total price done

---
+++ total_price.js
@@ -0,0 +1,18 @@
+var phones=[{name:'lava',camera:'8mpx',ram:'4gb',price:13500},
+            {name:'symphony',camera:'8mpx',ram:'4gb',price:11500},
+            {name:'sumsang',camera:'48mpx',ram:'6gb',price:19500},
+            {name:'iphone',camera:'12mpx',ram:'16gb',price:82200},
+            {name:'nokia',camera:'10mpx',ram:'8gb',price:42000},
+            {name:'xiomy',camera:'24mpx',ram:'6gb',price:22000},
+            {name:'pocco',camera:'8mpx',ram:'2gb',price:8000},]; 
+function chepest_phone(phone){
+    var total_price=0;
+    for(var i=0;i<phone.length;i++){
+        var value=phone[i];
+        // console.log(value);
+        total_price=total_price+value.price;
+    }
+    return total_price;
+}
+var result=chepest_phone(phones);
+console.log(result);
